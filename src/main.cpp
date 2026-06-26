470a023596f1c9c07021ac646f9d8faff2af2e99
ellakesch <ellakesch@gmail.com>
2026-06-27 00:34:40 +0200

    reafctor: edit main function to implement Intersection structure and display intersections wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
 
-int main()
-{
-    std::cout<<"City Optimizer - online \n";    
+struct Intersection{
+    int id;
+    std::string name;
+};
+
+int main() {
+    Intersection inter1 = {1, "Huhnerpostenn"};
+    Intersection inter2 = {2, "haakestrasse"};
+
+    std::vector<Intersection> intersections;
+    intersections.push_back(inter1);
+    intersections.push_back(inter2);
+
+    for (const auto& item : intersections) {
+        std::cout << "Intersection ID: " << item.id 
+                  << ", Name: " << item.name 
+                  << "\n ";
+    }
+
     return 0;
 }
